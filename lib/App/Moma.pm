df096ad0e4d456a4288154f454a1b0fdf3d4120a
stenington <mlarsson@mlarsson-laptop.coderyte.ad>
2010-08-09 18:18:27 -0400

    reorganizing code

--- lib/App/Moma.pm
+++ lib/App/Moma.pm
@@ -8,17 +8,21 @@ our $VERSION = '0.0.2';
 use Config::Tiny;
 use Moose;
 
-has 'ports_on' => (
+# Public
+
+has 'mirror' => (
   is => 'rw',
-  isa => 'ArrayRef[Str]',
-  default => sub{ [] },
+  isa => 'Bool',
+  default => 0,
 );
 
-has 'ports_off' => (
+has 'verbosity' => (
   is => 'rw',
-  isa => 'ArrayRef[Str]',
-  default => sub{ [] },
+  isa => 'Int',
+  default => 0,
 );
+
+# Private-ish
 
 has '_config_file' => (
   is => 'rw',
@@ -29,18 +33,6 @@ has '_config' => (
   is => 'ro',
   isa => 'Config::Tiny',
   writer => '_set_config',
-);
-
-has 'verbosity' => (
-  is => 'rw',
-  isa => 'Int',
-  default => 0,
-);
-
-has 'mirror' => (
-  is => 'rw',
-  isa => 'Bool',
-  default => 0,
 );
 
 has 'modes' => (
@@ -55,15 +47,26 @@ has 'portnames' => (
   default => sub{ {} },
 );
 
+has 'ports_on' => (
+  is => 'rw',
+  isa => 'ArrayRef[Str]',
+  default => sub{ [] },
+);
+
+has 'ports_off' => (
+  is => 'rw',
+  isa => 'ArrayRef[Str]',
+  default => sub{ [] },
+);
+
 sub load_rc {
   my ($self, $cfgfile) = @_;
 
   unless( $cfgfile ){
     $cfgfile = $ENV{HOME} . "/.moma";
-    return 0 unless -f $cfgfile;
   }
-
   $cfgfile =~ s/^~/$ENV{HOME}/;
+
   my $cfg = Config::Tiny->read( $cfgfile ) or die "Unable to load $cfgfile: " . Config::Tiny::errstr;
   $self->portnames($cfg->{identifiers}) if $cfg->{identifiers};
   $self->modes($cfg->{modes}) if $cfg->{modes};
@@ -97,23 +100,6 @@ sub check_config {
   print $self->_check_config( $avail );
 }
 
-sub _check_config {
-  my ($self, $avail) = @_;
-  my $msg = "";
-  foreach my $id (keys %{$self->portnames}) {
-    my $port = $self->portnames->{$id};
-    $msg .= "$id=$port: $port not connected\n" unless $avail->{$port};
-  }
-  foreach my $port (keys %{$self->modes}) {
-    my $mode = $self->modes->{$port};
-    unless( $avail->{$port}->{$mode} ){
-      $msg .= "$port=$mode: $mode not available for $port\n";
-      $msg .= "  available modes are ".join(", ", keys %{$avail->{$port}})."\n";
-    }
-  }
-  return $msg || "Looks good!\n";
-}
-
 sub _parse_available {
   my ($self, $query) = @_;
   my @lines = split('\n', $query);
@@ -133,6 +119,23 @@ sub _parse_available {
   return $avail;
 }
 
+sub _check_config {
+  my ($self, $avail) = @_;
+  my $msg = "";
+  foreach my $id (keys %{$self->portnames}) {
+    my $port = $self->portnames->{$id};
+    $msg .= "$id=$port: $port not connected\n" unless $avail->{$port};
+  }
+  foreach my $port (keys %{$self->modes}) {
+    my $mode = $self->modes->{$port};
+    unless( $avail->{$port}->{$mode} ){
+      $msg .= "$port=$mode: $mode not available for $port\n";
+      $msg .= "  available modes are ".join(", ", keys %{$avail->{$port}})."\n";
+    }
+  }
+  return $msg || "Looks good!\n";
+}
+
 sub parse_args {
   my ($self, $str) = @_;
   my ($on, $off) = $self->_parse($str);
@@ -141,16 +144,6 @@ sub parse_args {
   $self->ports_on( $ports_on );
   $self->ports_off( $ports_off );
   return @{$self->ports_on} + @{$self->ports_off};
-}
-
-sub _map_to_portnames {
-  my ($self, $ids) = @_;
-  my $portnames = [];
-  foreach my $mon (split(//, $ids)) {
-    die "Unknown identifier $mon" unless $self->portnames->{$mon};
-    push @$portnames, $self->portnames->{$mon};
-  }
-  return $portnames;
 }
 
 sub _parse {
@@ -176,20 +169,20 @@ sub _parse {
   return ($on, $off);
 }
 
+sub _map_to_portnames {
+  my ($self, $ids) = @_;
+  my $portnames = [];
+  foreach my $mon (split(//, $ids)) {
+    die "Unknown identifier $mon" unless $self->portnames->{$mon};
+    push @$portnames, $self->portnames->{$mon};
+  }
+  return $portnames;
+}
+
 sub run {
   my ($self) = @_;
   my $cmd = $self->_build_cmd($self->ports_on, $self->ports_off, $self->modes);
   $self->_call( $cmd );
-}
-
-sub _call {
-  my ($self, $cmd) = @_;
-
-  print "I'm gonna call [$cmd]!\n" if $self->verbosity > 0;
-  my $err = system $cmd;
-  if( $err ){
-    print STDERR "That didn't work, sorry.\n";
-  }
 }
 
 sub _build_cmd {
@@ -209,6 +202,16 @@ sub _build_cmd {
     $cmd .= "--output $mon --off ";
   }
   return $cmd;
+}
+
+sub _call {
+  my ($self, $cmd) = @_;
+
+  print "I'm gonna call [$cmd]!\n" if $self->verbosity > 0;
+  my $err = system $cmd;
+  if( $err ){
+    print STDERR "That didn't work, sorry.\n";
+  }
 }
 
 no Moose;
